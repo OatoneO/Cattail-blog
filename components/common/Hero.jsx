e010d56ef29ca9efcf685eadb56c1e1058d68edf
Cattail <728293924@qq.com>
2025-05-09 21:47:21 +0800

    style(common): 调整 Hero 组件中的颜色和样式
    - 修改了 Hi, I'm Cattail 文字的背景渐变色
    - 更新了技能标签的背景渐变色
    - 调整了个人信息文本的背景渐变色和透明度

--- components/common/Hero.jsx
+++ components/common/Hero.jsx
@@ -5,18 +5,18 @@ export default function Hero() {
   return (
     <div>
       <p className="mb-6 font-semibold">
-        <span className="text-transparent sm:bg-gradient-to-r to-foreground bg-gradient-to-t from-muted-foreground bg-clip-text lg:text-[54px] text-[40px]">
+        <span className="text-transparent sm:bg-gradient-to-r to-foreground bg-gradient-to-t from-foreground bg-clip-text lg:text-[54px] text-[40px]">
           Hi, I&apos;m Cattail
         </span>
       </p>
       <div className="h-10 mb-8 sm:mb-10">
         <HeroAnimation text1={"<Developer />"} text2="<Student />" />
       </div>
-      <p className="mb-8 text-xl text-transparent sm:mb-10 sm:text-[26px] bg-gradient-to-r from-green-200 via-green-100 opacity-60 to-green-200 bg-clip-text w-fit">
+      <p className="mb-8 text-xl sm:mb-10 sm:text-[26px] text-transparent bg-gradient-to-r from-green-400 via-green-300 to-green-400 bg-clip-text w-fit">
         #Frontend #Backend #Fullstack
       </p>
 
-      <p className="mb-4 text-sm text-transparent sm:mb-6 sm:text-base bg-gradient-to-b to-muted-foreground from-foreground bg-clip-text">
+      <p className="mb-4 text-sm sm:mb-6 sm:text-base text-transparent bg-gradient-to-b to-foreground/80 from-foreground bg-clip-text">
         I&apos;m an undergraduate student at NJTECH 
         <br />
         majoring in Computer Science and Technology.
